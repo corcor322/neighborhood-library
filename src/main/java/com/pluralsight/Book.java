f12dcb86307baddc1b9341813201a77336086a0e
corcor322 <corcor322@gmail.com>
2024-10-09 17:03:05 -0400

    Added attributes for Book class and their default values

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
 public class Book {
+    private String title = "";
+    private String author ="";
+    private String callNumber = "";
+    private long isbn = 0;
+    private boolean isCheckedOut = false;
+    private String checkedOutTo = "N/A";
 }
