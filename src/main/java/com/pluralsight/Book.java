86c0f622a144b1f58ac4797a9c14d37012f830e2
corcor322 <corcor322@gmail.com>
2024-10-09 17:13:41 -0400

    Added constructor for book class, added getters and setters for private attributes

--- src/main/java/com/pluralsight/Book.java
+++ src/main/java/com/pluralsight/Book.java
@@ -7,4 +7,61 @@ public class Book {
     private long isbn = 0;
     private boolean isCheckedOut = false;
     private String checkedOutTo = "N/A";
+
+    public Book(String title, String author, String callNumber, long isbn, boolean isCheckedOut, String checkedOutTo) {
+        this.title = title;
+        this.author = author;
+        this.callNumber = callNumber;
+        this.isbn = isbn;
+        this.isCheckedOut = isCheckedOut;
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getCallNumber() {
+        return callNumber;
+    }
+
+    public void setCallNumber(String callNumber) {
+        this.callNumber = callNumber;
+    }
+
+    public long getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(long isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isCheckedOut() {
+        return isCheckedOut;
+    }
+
+    public void setCheckedOut(boolean checkedOut) {
+        isCheckedOut = checkedOut;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
 }
